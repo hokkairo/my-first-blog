b7e2dc40ea85d09879e253f4e9120cddb74328ca
hokkairo <kem2soc.1533@gmail.com>
2021-02-09 00:04:11 +0900

    Changed the HTML for the site.

---
+++ blog/urls.py
@@ -0,0 +1,15 @@
+
+from django.urls import path
+from . import views
+'''
+これはDjangoのpath関数と、
+blogアプリの全てのvievsをインポートするという意味です。
+'''
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+]
+'''
+このパターンは誰かがあなたのWebサイトの'http://127.0.0.1:8000/'という
+アドレスにアクセスしてきたらviews.post_list が正しい行き先だということをDjangoに伝えます。
+'''
