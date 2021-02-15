a06dcdc336a8d85e126c7c4a8118d5672ca957f1
hokkairo <kem2soc.1533@gmail.com>
2021-02-15 22:48:18 +0900

    Added views to create/edit blog post inside the site.

--- blog/urls.py
+++ blog/urls.py
@@ -9,6 +9,8 @@ blogアプリの全てのvievsをインポートするという意味です。
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
 '''
 このパターンは誰かがあなたのWebサイトの'http://127.0.0.1:8000/'という
